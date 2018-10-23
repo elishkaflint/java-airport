483d3ca21c5223551d3a65ff074006c6f97dca7c
Elishka Flint <eflint@uk-c02x4e0jjg5j.uk.deloitte.com>
2018-10-23 14:06:16 +0100

    Add basic intro method

--- src/main/java/airport/App.java
+++ src/main/java/airport/App.java
@@ -4,10 +4,11 @@ package airport;
  * Hello world!
  *
  */
-public class App 
+
+public class App
 {
-    public static void main( String[] args )
-    {
-        System.out.println( "Hello World!" );
+    public String concatAndConvertString(String str1, String str2){
+        String concatedString=str1.concat(str2);
+        return concatedString.toUpperCase();
     }
 }
