67f9ed984e284a77706f78535f5725a424072f55
Elishka Flint <eflint@uk-c02x4e0jjg5j.uk.deloitte.com>
2018-10-23 19:23:14 +0100

    Add ground method in plane class to pass test

--- src/main/java/airport/Plane.java
+++ src/main/java/airport/Plane.java
@@ -1,4 +1,14 @@
 package airport;
 
 public class Plane {
+
+    boolean isGrounded;
+
+    public Plane() {
+        this.isGrounded = false;
+    }
+
+    public void ground() {
+        this.isGrounded = true;
+    }
 }
