43c2a9373af442cb007cdcadb9b68d91ede66399
Elishka Flint <eflint@uk-c02x4e0jjg5j.uk.deloitte.com>
2018-10-23 15:13:52 +0100

    Add test for raising exception when airport is full

--- src/main/test/airport/AirportTest.java
+++ src/main/test/airport/AirportTest.java
@@ -1,16 +1,31 @@
 package airport;
 
 import org.junit.Test;
+import org.junit.Rule;
+import org.junit.rules.ExpectedException;
 
 import static org.junit.Assert.assertTrue;
 
 public class AirportTest {
 
     @Test
-    public void testLand() throws Exception {
+    public void testLand() {
         Airport airport = new Airport();
         airport.land("plane");
         assertTrue(airport.planes.contains("plane"));
+    }
+
+    @Rule
+    public ExpectedException exceptionRule = ExpectedException.none();
+
+    @Test
+    public void testTooManyLandingsRaisesError() {
+        exceptionRule.expect(RuntimeException.class);
+        exceptionRule.expectMessage("Airport is full!");
+        Airport airport = new Airport();
+        airport.land("plane");
+        airport.land("plane");
+        airport.land("plane");
     }
 
 }
